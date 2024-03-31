9fa46371dc7ae4f2d8c214da40e4fa498d5ab434
Komil <komilovkomil156@gmail.com>
2024-03-31 17:17:26 +0300

    homework git test

---
+++ src/main/java/CollectionsHomeWork/Main.java
@@ -0,0 +1,7 @@
+package CollectionsHomeWork;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is the most important home work in my life");
+    }
+}
