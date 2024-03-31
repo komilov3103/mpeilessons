9d1dfcf69547074d1a0a36004db6c5b36387f6f2
Komil <komilovkomil156@gmail.com>
2024-03-31 17:18:39 +0300

    new commit test

--- src/main/java/CollectionsHomeWork/Main.java
+++ src/main/java/CollectionsHomeWork/Main.java
@@ -3,5 +3,6 @@ package CollectionsHomeWork;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is the most important home work in my life");
+        System.out.println("One more commit test");
     }
 }
